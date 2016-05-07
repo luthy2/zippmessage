6762d95b4b941f8929df818c9e129de306346709
Justin Luthy <Justin@Justins-MacBook-Pro.local>
2016-05-06 21:09:26 -0700

    bug fix

--- app/views.py
+++ app/views.py
@@ -417,4 +417,4 @@ def tags():
 
 
 def redirect_url(default='index'):
-	return request.args.get('next') or request.referrer or url_for(default)
+	return request.args.get('next') or request.referrer or default
