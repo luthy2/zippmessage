2094c8a83e8be62b77f1b1a3884d5dd8ebe4ebfd
Justin Luthy <Justin@Justins-MacBook-Pro.local>
2016-07-10 16:35:28 -0700

    api inbox

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -6,7 +6,7 @@ zippApp.controller("InboxController", function InboxController($scope, $http, $q
   $scope.inbox = [
     $http ({
       method: 'GET',
-      url: 'http://zippmessage-staging.herokuapp.com/heartbeat'
+      url: 'http://zippmessage-staging.herokuapp.com/api/1/heartbeat'
     }).then(function success(response){
       console.log('response')
     }), function error(response){
