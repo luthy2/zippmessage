dfe0b00b0ba95d6fe4c253536993ef78c7f72bcc
Justin Luthy <Justin@Justins-MacBook-Pro.local>
2016-07-10 15:16:10 -0700

    api inbox

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,17 +1,17 @@
-var zippApp = angluar.module('zippApp', []);
+var zippApp = angluar.module("zippApp", []);
 
-zippApp.controller('InboxController', function InboxController($scope){
+zippApp.controller("InboxController", function InboxController($scope){
   $scope.inbox = [
     {
       title:"A Really Interesting Article",
       url:"https://www.google.com",
-      from_user:"@John",
+      fromUser:"@John",
       description:"This article details some really interesting information",
       note:"read this article!!"
     },{
       title:"A Cool Article",
       url:"https://www.twitter.com",
-      from_user:"@Mike",
+      fromUser:"@Mike",
       description:"This article details some cool information",
       note:"cool article!!"
     }
