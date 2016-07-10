a596f2cb59f3606ef6d44cd25084da2dcf0c337f
Justin Luthy <Justin@Justins-MacBook-Pro.local>
2016-07-10 16:28:33 -0700

    api inbox

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -4,18 +4,13 @@ var zippApp = angular.module("zippApp", []);
 
 zippApp.controller("InboxController", function InboxController($scope){
   $scope.inbox = [
-    {
-      title:'A Really Interesting Article',
-      url:"https://www.google.com",
-      fromUser:"@John",
-      description:"This article details some really interesting information",
-      note:"read this article!!"
-    }, {
-      title:"A Cool Article",
-      url:"https://www.twitter.com",
-      fromUser:"@Mike",
-      description:"This article details some cool information",
-      note:"cool article!!"
+    $http ({
+      method: 'GET',
+      url: 'http://zippmessage-staging.herokuapp.com/heartbeat'
+    }).then(function success(response){
+      console.log('response')
+    }), function error(response){
+      console.log('response')
     }
   ];
 });
