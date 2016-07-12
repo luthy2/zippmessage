f740cc2006275b66889be95f0b9ea732ae953674
Justin Luthy <Justin@Justins-MacBook-Pro.local>
2016-07-12 12:11:49 -0700

    api inbox

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -14,18 +14,18 @@ zippApp.controller("InboxController", function InboxController($scope, $http, $q
       console.log(response)
     };
 
-    $scope.bookmarkMessage = function(messageId){
-        $http ({
-          method: 'GET',
-          url: 'http://zippmessage-staging.herokuapp.com/api/1/bookmark/'+= messageId.toString()
-        }).then(function success(response){
-          console.log(response)
-        }), function error(response){
-          console.log(response)
-        };
+  $scope.bookmarkMessage = function(messageId){
+      $http ({
+        method: 'GET',
+        url: 'http://zippmessage-staging.herokuapp.com/api/1/bookmark/'+= messageId
+      }).then(function success(response){
+        console.log(response)
+      }), function error(response){
+        console.log(response)
       };
+    };
 
-    $scope.dissmissMessage = function(messageId){
-      alert('Message Id is ' + messageId)
-      };
+  $scope.dissmissMessage = function(messageId){
+    alert('Message Id is ' + messageId)
+    };
 });
