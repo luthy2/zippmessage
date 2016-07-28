9ef1095b1f4004f695990e6f498e140a727a6d92
Justin Luthy <Justin@Justins-MacBook-Pro.local>
2016-07-27 18:52:47 -0700

    ajax inbox

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -70,7 +70,9 @@ zippApp.controller("InboxController", function InboxController($scope, $http, $q
         $scope.alert = 'Message Dismissed'
         setTimeout(function(){
           $scope.alert = ''
+          console.log($scope.alert)
         }, 3000);
+        console.log($scope.alert)
     }), function error(response){
       console.log(response)
       alert('hmmm... something went wrong and we were unable to dismiss the message.')
