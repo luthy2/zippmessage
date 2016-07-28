d000193cab7b17fc5f399ad70f52f1583a945b64
Justin Luthy <Justin@Justins-MacBook-Pro.local>
2016-07-27 22:49:25 -0700

    ajax inbox

--- app/models.py
+++ app/models.py
@@ -275,7 +275,7 @@ def twitter_tag(url):
 
 def soundcloud_tag(url):
 	#custom rendering for soundcloud
-	resp = requests.get('https://api.soundcloud.com/oembed?&url=%s&format=json' % url)
+	resp = requests.get('https://api.soundcloud.com/oembed?url=%s' % url)
 	resp = resp.json()
 	if not 'error' in resp:
 		return resp['html']
