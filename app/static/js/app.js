36e262d0fd913a8b3d17e1815228fb74ffbb128d
Justin Luthy <Justin@Justins-MacBook-Pro.local>
2016-07-28 21:27:05 -0700

    ajax inbox

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -4,7 +4,7 @@ var zippApp = angular.module("zippApp", []);
 
 zippApp.controller("InboxController", function InboxController($scope, $http, $q, $sce, $timeout){
   $scope.alert = '';
-  $scope.hideAlert = true;
+  $scope.loading = true;
   $scope.$sce = $sce;
   $scope.loadedAll = false;
   $scope.inbox = []
@@ -14,6 +14,7 @@ zippApp.controller("InboxController", function InboxController($scope, $http, $q
     }).then(function success(response){
       console.log(response)
       $scope.inbox = response.data
+      $scope.loading = false;
       if (response.data.length < 6){
         $scope.loadedAll = true;
       }
@@ -22,6 +23,7 @@ zippApp.controller("InboxController", function InboxController($scope, $http, $q
     };
 
   $scope.getInbox = function(){
+    $scope.loading = true;
     $http ({
       method:'GET',
       url:'http://zippmessage-staging.herokuapp.com/api/1/user/inbox',
