1e791203c38654872a9997ceb5d801d2da9ef81d
Justin Luthy <Justin@Justins-MacBook-Pro.local>
2016-08-12 00:43:10 -0700

    async tasks, setup

--- app/views.py
+++ app/views.py
@@ -652,7 +652,7 @@ def send_new_msg_email(sender_id, recipient_id, message_id):
 		url = message.url
 		url = url.encode('utf-8')
 		content = bm.get(url) or message.render_url()
-		html = render_template('new_message_email.html', sender = sender, recipient = recipient, note = message.title, content = content, timedelta = message.format_timestamp())
+		html = render_template('new_message_email.html', sender = sender.username, recipient = recipient.username, note = message.title, content = content, timedelta = message.format_timestamp())
 		if html:
 			print 'html success'
 		if r_email:
