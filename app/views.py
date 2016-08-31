6979a24edb61a6a6809dc5913eae1811a22e9bdc
Justin Luthy <Justin@Justins-MacBook-Pro.local>
2016-08-31 16:55:19 -0700

    find friends from twitter

--- app/views.py
+++ app/views.py
@@ -168,7 +168,7 @@ def find_contacts():
 	session.pop("oauth_token", {})
 	session.pop("oauth_secret", {})
 	session.pop("oauth_token_secret", {})
-	sessopn.pop('twitter_token', {})
+	session.pop('twitter_token', {})
 	user = g.user
 	s = time.time()
 	f = twitter.get('friends/ids.json',data ={'screen_name':str(user.username)})
