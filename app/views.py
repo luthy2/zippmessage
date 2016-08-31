a9e23173d69bd8c6ae123d70525f3ad7e9baed46
Justin Luthy <Justin@Justins-MacBook-Pro.local>
2016-08-31 16:25:33 -0700

    find friends from twitter

--- app/views.py
+++ app/views.py
@@ -167,7 +167,7 @@ def contacts():
 def find_contacts():
 	user = g.user
 	s = time.time()
-	f = twitter.get('/friends/ids.json',data ={'screen_name':str(user.username)})
+	f = twitter.get('friends/ids.json',data ={'screen_name':str(user.username)})
 	f = twitter.post("/users/", data = {'user_id':f['ids']})
 	e = time.time()
 	print "data from twitter in", s-e
