e9fef68443f03614d343d80791d363a1e5cc4b3d
Justin Luthy <Justin@Justins-MacBook-Pro.local>
2016-09-11 18:14:07 -0700

    masonry layout for bookmarks

--- app/views.py
+++ app/views.py
@@ -507,7 +507,7 @@ def message_reader(message_id):
 
 @app.route("/explore", methods = ["GET", "POST"] )
 def explore():
-	bot = User.query.filter(User.username.ilike('zippbot'))
+	bot = User.query.filter(User.username.ilike('zippbot')).first()
 	m = UserMessage.query.filter(UserMessage.user_id==bot.id).order_by(UserMessage.message_id.desc()).limit(30).all()
 	items = []
 	for i in m:
