d169848c6ccff1aa69c84972a9869f9cd358874a
Justin Luthy <Justin@Justins-MacBook-Pro.local>
2016-09-14 17:42:58 -0700

    security issue

--- app/views.py
+++ app/views.py
@@ -509,6 +509,7 @@ def reader(page = 1):
 def message_reader(message_id):
 	user = g.user
 	m = Message.query.get(message_id)
+	m = UserMessage.query.filter(UserMessage.message_id == m.id).filter(UserMessage.user_id == user.id).first()
 	return render_template('message_reader.html', user = user, title = 'Reader', message = m)
 
 
