8bbc1bbf99fd2484e937e8116b513309b7b1580a
Justin Luthy <Justin@Justins-MacBook-Pro.local>
2016-09-15 18:30:23 -0700

    activity test

--- app/models.py
+++ app/models.py
@@ -145,7 +145,7 @@ class User(db.Model):
 		return self
 
 	def user_activity(self):
-		activity = Activity.query.filter(Activity.owner_id == self.id).limit(50).order_by(self.timestamp.desc())
+		activity = Activity.query.filter(Activity.owner_id == self.id).limit(50).order_by(Activity.timestamp.desc())
 		return activity
 
 	def create_activity(self, owner_id, action, message_id, timestamp = datetime.utcnow()):
