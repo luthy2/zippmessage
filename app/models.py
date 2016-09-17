e792764f56378c313471769e63317fbb47b1b10f
Justin Luthy <Justin@Justins-MacBook-Pro.local>
2016-09-16 19:37:32 -0700

    reactions bug fixes

--- app/models.py
+++ app/models.py
@@ -149,7 +149,7 @@ class User(db.Model):
 		return activity
 
 	def create_activity(self, owner_id, action, message_id, timestamp = datetime.utcnow()):
-		exists = Activity.query.filter(Activtiy.message_id == message_id).filter(Activity.subject_id == self.id).filter(Activity.action==action).scalar()
+		exists = Activity.query.filter(Activity.message_id == message_id).filter(Activity.subject_id == self.id).filter(Activity.action==action).scalar()
 		if exists is None:
 			a = Activity(owner_id = owner_id, subject_id = self.id, action = action, message_id = message_id, timestamp = timestamp)
 			db.session.add(a)
