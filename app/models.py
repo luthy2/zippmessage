4f01ff23aa31c36fcc39fd0402e2a4868b98eba5
Justin Luthy <Justin@Justins-MBP.ptp>
2016-09-19 16:33:50 -0700

    added activity to dashboard

--- app/models.py
+++ app/models.py
@@ -433,44 +433,44 @@ def get_url_content(message_url):
 			return render_no_style(message_url)
 
 
-# class Activity(db.Model):
-# 	id = db.Column('activity_id', db.Integer, primary_key = True)
-# 	owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key = True) #index for construction of feeds
-# 	subject_id = db.Column(db.Integer, db.ForeignKey('user.id'),  primary_key=True) #who performed the action
-# 	action = db.Column(db.String()) #the type of action
-# 	message_id = db.Column(db.Integer, db.ForeignKey("message.id"), primary_key=True) #the message the action was performed on
-# 	timestamp = db.Column(db.DateTime) #when
-#
-# 	def __init__(self, owner_id, subject_id, action, message_id, timestamp):
-# 		self.owner_id = owner_id #who receives the activity
-# 		self.subject_id = subject_id #who performed the action
-# 		self.action = action
-# 		self.message_id = message_id
-# 		self.timestamp = timestamp
-#
-# 	def __repr__(self):
-# 		return '<Activity %r>' % (self.action)
-#
-# 	def format_timestamp(self):
-# 		ts = self.timestamp
-# 		now = datetime.utcnow()
-# 		tdelta = now - ts
-# 		s = tdelta.total_seconds()
-# 		if s <= 59:
-# 			s = s//1
-# 			return '{0}s'.format(s)
-# 		elif 60 <= s < 3600:
-# 			s = s//60
-# 			return '{0}m ago'.format(int(s))
-# 		elif 3600 <= s < 86400:
-# 			s = s//3600
-# 			return '{0}h ago'.format(int(s))
-# 		elif 86400 <= s < 604800:
-# 			s = s//86400
-# 			return '{0}d ago'.format(int(s))
-# 		else:
-# 			s = s//604800
-# 			return '{0}w ago'.format(int(s))
+class Activity(db.Model):
+	id = db.Column('activity_id', db.Integer, primary_key = True)
+	owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key = True) #index for construction of feeds
+	subject_id = db.Column(db.Integer, db.ForeignKey('user.id'),  primary_key=True) #who performed the action
+	action = db.Column(db.String()) #the type of action
+	message_id = db.Column(db.Integer, db.ForeignKey("message.id"), primary_key=True) #the message the action was performed on
+	timestamp = db.Column(db.DateTime) #when
+
+	def __init__(self, owner_id, subject_id, action, message_id, timestamp):
+		self.owner_id = owner_id #who receives the activity
+		self.subject_id = subject_id #who performed the action
+		self.action = action
+		self.message_id = message_id
+		self.timestamp = timestamp
+
+	def __repr__(self):
+		return '<Activity %r>' % (self.action)
+
+	def format_timestamp(self):
+		ts = self.timestamp
+		now = datetime.utcnow()
+		tdelta = now - ts
+		s = tdelta.total_seconds()
+		if s <= 59:
+			s = s//1
+			return '{0}s'.format(s)
+		elif 60 <= s < 3600:
+			s = s//60
+			return '{0}m ago'.format(int(s))
+		elif 3600 <= s < 86400:
+			s = s//3600
+			return '{0}h ago'.format(int(s))
+		elif 86400 <= s < 604800:
+			s = s//86400
+			return '{0}d ago'.format(int(s))
+		else:
+			s = s//604800
+			return '{0}w ago'.format(int(s))
 
 # class Action(db.Model):
 # 	id = db.Column(db.Integer, primary_key = True)
