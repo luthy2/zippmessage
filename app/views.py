3339f420c9d116e0d985099a8217602f4c64709e
Justin Luthy <Justin@Justins-MacBook-Pro.local>
2016-10-01 14:29:19 -0700

    New Profile Layout

--- app/views.py
+++ app/views.py
@@ -223,11 +223,15 @@ def find_contacts():
 @login_required
 def user(username):
 	_user = User.query.filter(User.username.ilike(username)).first()
+	if _user == g.user:
+		history = Message.query.filter(Message.author==user).order_by(Message.timestamp.desc()).limit(50)
+	else:
+		history=None
 	if _user:
 		tags=_user.tags_for_user()
 	else:
 		tags=None
-	return render_template('user.html', user = _user, tags = tags, title = 'Profile', inbox=inbox)
+	return render_template('user.html', user = _user, tags = tags, title = 'Profile', inbox=inbox, history = history)
 
 @app.route('/user/<username>/edit', methods = ["GET", "POST"])
 @login_required
